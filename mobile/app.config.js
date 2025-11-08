38cb4ddf95ec83b87c219b5e0fc30e66821186b6
samjoshuadud <calebjoshuaarmojallas@gmail.com>
2025-11-08 19:02:25 +0800

    Fix Build JSON Secrets EAS 2

--- mobile/app.config.js
+++ mobile/app.config.js
@@ -1,87 +1,67 @@
-import fs from "fs";
-
-export default ({ config }) => {
-  if (process.env.GOOGLE_SERVICES_JSON) {
-    fs.writeFileSync(
-      'android/app/google-services.json',
-      process.env.GOOGLE_SERVICES_JSON,
-      { encoding: 'utf8' }
-    );
-    console.log('✅ google-services.json written');
-  }
-
-  if (process.env.GOOGLE_SERVICE_INFO_PLIST) {
-    fs.writeFileSync(
-      'ios/GoogleService-Info.plist',
-      process.env.GOOGLE_SERVICE_INFO_PLIST,
-      { encoding: 'utf8' }
-    );
-    console.log('✅ GoogleService-Info.plist written');
-  }
-
-  return {
-    ...config,
-    name: "Seated",
-    slug: "mobile",
-    owner: "softendseated",
-    version: "1.0.0",
-    orientation: "portrait",
-    icon: "./assets/images/icon-logo.png",
-    scheme: "mobile",
-    userInterfaceStyle: "automatic",
-    newArchEnabled: true,
-    ios: {
-      supportsTablet: true,
-      bundleIdentifier: "com.softendseated.mobile",
+export default ({ config }) => ({
+  ...config,
+  name: "Seated",
+  slug: "mobile",
+  owner: "softendseated",
+  version: "1.0.0",
+  orientation: "portrait",
+  icon: "./assets/images/icon-logo.png",
+  scheme: "mobile",
+  userInterfaceStyle: "automatic",
+  newArchEnabled: true,
+  ios: {
+    supportsTablet: true,
+    bundleIdentifier: "com.softendseated.mobile",
+    googleServicesFile: process.env.GOOGLE_SERVICE_INFO_PLIST,
+  },
+  android: {
+    adaptiveIcon: {
+      backgroundColor: "#E6F4FE",
+      foregroundImage: "./assets/images/android-icon-foreground.png",
+      backgroundImage: "./assets/images/android-icon-background.png",
+      monochromeImage: "./assets/images/android-icon-monochrome.png",
     },
-    android: {
-      adaptiveIcon: {
-        backgroundColor: "#E6F4FE",
-        foregroundImage: "./assets/images/android-icon-foreground.png",
-        backgroundImage: "./assets/images/android-icon-background.png",
-        monochromeImage: "./assets/images/android-icon-monochrome.png",
-      },
-      edgeToEdgeEnabled: true,
-      predictiveBackGestureEnabled: false,
-      package: "com.softendseated.seated",
+    edgeToEdgeEnabled: true,
+    predictiveBackGestureEnabled: false,
+    package: "com.softendseated.seated",
+    googleServicesFile: process.env.GOOGLE_SERVICES_JSON,
+  },
+  web: {
+    output: "static",
+    favicon: "./assets/images/favicon.png",
+  },
+  extra: {
+    FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+    FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
+    FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+    FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
+    GOOGLE_WEB_CLIENT_ID: process.env.GOOGLE_WEB_CLIENT_ID,
+    API_URL: process.env.API_URL,
+    EXPO_USERNAME: process.env.EXPO_USERNAME,
+    eas: {
+      projectId: "8711db8e-b932-464b-827b-888c31943f30",
     },
-    web: {
-      output: "static",
-      favicon: "./assets/images/favicon.png",
-    },
-    extra: {
-      FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
-      FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
-      FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
-      FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
-      GOOGLE_WEB_CLIENT_ID: process.env.GOOGLE_WEB_CLIENT_ID,
-      API_URL: process.env.API_URL,
-      EXPO_USERNAME: process.env.EXPO_USERNAME,
-      eas: {
-        projectId: "8711db8e-b932-464b-827b-888c31943f30",
-      },
-    },
-    plugins: [
-      "expo-router",
-      [
-        "expo-splash-screen",
-        {
-          image: "./assets/images/splash-icon.png",
-          imageWidth: 200,
-          resizeMode: "contain",
-          backgroundColor: "#ffffff",
-          dark: {
-            backgroundColor: "#000000",
-          },
+  },
+  plugins: [
+    "expo-router",
+    [
+      "expo-splash-screen",
+      {
+        image: "./assets/images/splash-icon.png",
+        imageWidth: 200,
+        resizeMode: "contain",
+        backgroundColor: "#ffffff",
+        dark: {
+          backgroundColor: "#000000",
         },
-      ],
-      "expo-font",
-      "@react-native-google-signin/google-signin",
+      },
     ],
-    experiments: {
-      typedRoutes: true,
-      reactCompiler: true,
-    },
-  };
-};
+    "expo-font",
+    "@react-native-google-signin/google-signin",
+  ],
+  experiments: {
+    typedRoutes: true,
+    reactCompiler: true,
+  },
+});
 
